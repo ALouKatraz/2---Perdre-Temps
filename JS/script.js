eb6f186f1b59ab457b7137d1468725c9b6d94c50
ALouKatraz <louise.casteleyn@gmail.com>
2024-04-04 16:14:46 +0200

    Ajout d'un zéro si inférieur à 10

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,9 @@ let date = new Date();
 let heure = date.getHours();
 let minute = date.getMinutes();
 
+heure = heure < 10 ? "0" + heure : heure;
+minute = minute < 10 ? "0" + minute : minute;
+
 let count = minute;
 
 document.getElementById("heure").innerHTML = heure + ":" + minute;
