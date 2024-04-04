7c5222d0d5e33732fcf85e5242597358252d0eab
ALouKatraz <louise.casteleyn@gmail.com>
2024-04-04 16:26:03 +0200

    Modification de l'heure si les minutes sont supérieures à 59

--- JS/script.js
+++ JS/script.js
@@ -5,12 +5,17 @@ let minute = date.getMinutes();
 heure = heure < 10 ? "0" + heure : heure;
 minute = minute < 10 ? "0" + minute : minute;
 
-let count = minute;
-
 document.getElementById("heure").innerHTML = heure + ":" + minute;
+
+let count = minute;
+let count2 = heure;
 
 function time() {
   count = count + 1;
-  document.getElementById("heure").innerHTML = heure + ":" + count;
+  if (count > 59) {
+    count = 0; 
+    count2++;
+  }
+  document.getElementById("heure").innerHTML = count2 + ":" + count;
 }
 document.getElementById("time").addEventListener("click", time);
