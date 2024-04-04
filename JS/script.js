73faadf9e11fddc1a3fd340c9b2ee638bad0c9dc
ALouKatraz <louise.casteleyn@gmail.com>
2024-04-04 15:37:50 +0200

    Création de la fonction pour ajouter 1 minute au clic

--- JS/script.js
+++ JS/script.js
@@ -2,5 +2,13 @@ let date = new Date();
 let heure = date.getHours();
 let minute = date.getMinutes();
 
+let count = 0;
+
 document.getElementById("heure").innerHTML = heure + ":" + minute;
+
+function time() {
+  count = minute + 1;
+  document.getElementById("heure").innerHTML = heure + ":" + count;
+}
+document.getElementById("time").addEventListener("click", time);
 
