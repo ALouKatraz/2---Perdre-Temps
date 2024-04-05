9f122632472c6c38290dca1a713d17a3d7b216c3
ALouKatraz <louise.casteleyn@gmail.com>
2024-04-05 13:03:54 +0200

    Modification heure si minutes > 59 & correction bugs

--- JS/script.js
+++ JS/script.js
@@ -2,20 +2,26 @@ let date = new Date();
 let heure = date.getHours();
 let minute = date.getMinutes();
 
-heure = heure < 10 ? "0" + heure : heure;
-minute = minute < 10 ? "0" + minute : minute;
-
-document.getElementById("heure").innerHTML = heure + ":" + minute;
-
 let count = minute;
 let count2 = heure;
 
+count2 = count2 < 10 ? "0" + count2 : count2;
+count = count < 10 ? "0" + count : count;
+
+document.getElementById("heure").innerHTML = count2 + ":" + count;
+
 function time() {
-  count = count + 1;
+  count = parseInt(count) + 1;
   if (count > 59) {
-    count = 0; 
+    count = 0;
     count2++;
   }
+  count2 = parseInt(count2);
+  if (count2 > 23) {
+    count2 = 0;
+  }
+  count2 = count2 < 10 ? "0" + count2 : count2;
+  count = count < 10 ? "0" + count : count;
   document.getElementById("heure").innerHTML = count2 + ":" + count;
 }
 document.getElementById("time").addEventListener("click", time);
